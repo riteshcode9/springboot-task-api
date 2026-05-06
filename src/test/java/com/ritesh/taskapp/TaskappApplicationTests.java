f9e78997790770a8a2a95a683d64b54a42264311
Ritesh Panta <riteshpanta9@gmail.com>
2026-05-06 14:10:03 -0700

    Initial commit - Spring Boot Task API

---
+++ src/test/java/com/ritesh/taskapp/TaskappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritesh.taskapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
